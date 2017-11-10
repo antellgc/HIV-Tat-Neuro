3615cdcb68e5cd5d558ef64ce3916950dc7075ae
antellgc <antellgc@gmail.com>
2017-11-09 21:27:08 -0500

    finished nb03

--- code/clinical_preprocess.py
+++ code/clinical_preprocess.py
@@ -12,20 +12,32 @@ import itertools
 from collections import Counter
 import time
 import matplotlib.pyplot as plt
-%matplotlib inline
 
 def import_redcap_data(redcap_path):
-    '''
-    Imports REDCAP .cvs-formatted data dump
-    '''
-    redcap_df = pd.read_csv(redcap_path, sep='\t')
+    '''Imports REDCAP .cvs-formatted data dump'''
+    redcap_df = pd.read_csv(redcap_path, sep='\t', low_memory=False)
     redcap_df.head()
     return redcap_df
 
 def select_clinical_parameters(df, cols):
-    '''
-    Choose the columns to keep as relevant clinical parameters
-    '''
-    clin_df = df[cols.keys()].rename(columns=cols)
+    '''Choose the columns to keep as relevant clinical parameters'''
+    clin_df = df[list(cols.keys())].rename(columns=cols)
     clin_df['DateOfVisit'] = pd.to_datetime(clin_df['DateOfVisit'])
     return clin_df
+
+def update_gds_data(GDS_df, clin_df)
+    visits = []
+    for idx, row in GDS_df.iterrows():
+        NEURO_PATIENT = row['PatientID']
+        NEURO_DATE = row['VisitDate']    
+        clin_info = clin_df[clin_df.Patient == NEURO_PATIENT][['Patient', 'Visit', 'DateOfVisit']]
+        clin_info2 = clin_info[clin_info.DateOfVisit == NEURO_DATE][['Patient', 'Visit', 'DateOfVisit']]
+        if clin_info2.empty:
+            for a, b in clin_info.dropna().iterrows():
+                days_difference = b.DateOfVisit - NEURO_DATE
+                if abs(days_difference.days) < 7:
+                    visits.append(b.Visit)
+        else:
+            visits.append(list(clin_info2['Visit'])[0]) 
+    GDS_df['Visit'] = visits
+    return GDS_df[['PatientID', 'Visit', 'VisitDate', 'GDS']]
